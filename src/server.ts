2959dec90be88cc1a2c777055a4aef9a5c03147e
ahmed arafa <70607269+Ahmed14112017@users.noreply.github.com>
2026-06-04 00:29:15 +0300

    fix PORT

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import app from "./app";
 
 import "./config/mail";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI!)
